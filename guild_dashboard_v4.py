c90da5fb32e5915a5d8120ba7c77a6ecb46989b1
he02141894-png <he02141894@gmail.com>
2026-08-28 12:57:48 +0800

    Update guild_dashboard_v4.py

--- guild_dashboard_v4.py
+++ guild_dashboard_v4.py
@@ -125,7 +125,7 @@ if is_admin:
         st.markdown("##### 🛡️ 16 格核心裝備細項面板")
         
         gear_inputs = {}
-        st.markdown("**武器 & 防具**")
+        st.markdown("**主武 & 防具**")
         g_col1, g_col2 = st.columns(2)
         with g_col1:
             gear_inputs["主武器"] = st.text_input("主武器", value=default_gears["主武器"])
